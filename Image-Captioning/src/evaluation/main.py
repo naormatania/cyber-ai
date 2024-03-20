dee8ec51a0eb94cae44011cf046ccdad7b3d7d32
naormatania <naorm1991@gmail.com>
2024-03-20 12:19:11 +0200

    Add CIDEer evaluation

--- Image-Captioning/src/evaluation/main.py
+++ Image-Captioning/src/evaluation/main.py
@@ -1,17 +1,50 @@
 from datasets import load_dataset
-from transformers import BlipProcessor, BlipForConditionalGeneration
+from transformers import BlipProcessor, BlipForConditionalGeneration, Blip2Processor, Blip2ForConditionalGeneration
 import torch
 import evaluate
+import os
 from tqdm import tqdm
 from PIL import Image
+from cider_scorer import CiderScorer
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 12
-BLIP_MODEL_ID = "Salesforce/blip-image-captioning-large"
 
-BLIP_MODEL = BlipForConditionalGeneration.from_pretrained(BLIP_MODEL_ID)
-BLIP_MODEL = BLIP_MODEL.to(DEVICE)
-BLIP_PROCESSOR = BlipProcessor.from_pretrained(BLIP_MODEL_ID)    
+def hf_blip_base():
+    model_id = "Salesforce/blip-image-captioning-base"
+    model = BlipForConditionalGeneration.from_pretrained(model_id)
+    model = model.to(DEVICE)
+    processor = BlipProcessor.from_pretrained(model_id)
+    return model, processor
+
+def hf_blip_large():
+    model_id = "Salesforce/blip-image-captioning-large"
+    model = BlipForConditionalGeneration.from_pretrained(model_id)
+    model = model.to(DEVICE)
+    processor = BlipProcessor.from_pretrained(model_id)
+    return model, processor
+
+def hf_blip2_opt_2_7b():
+    model_id = "Salesforce/blip2-opt-2.7b"
+    model = Blip2ForConditionalGeneration.from_pretrained(model_id)
+    model = model.to(DEVICE)
+    processor = Blip2Processor.from_pretrained(model_id)
+    return model, processor
+
+MODEL_INITIALIZERS = {
+   'hf-blip-base': hf_blip_base,
+#    'hf-blip-large': hf_blip_large,
+#    'hf-blip2-opt-2.7b': hf_blip2_opt_2_7b,
+}
+
+def caption_images(model, processor, image_paths):
+    predictions = []
+    for i in tqdm(range(0, len(image_paths), BATCH_SIZE)):
+        images = [Image.open(path) for path in image_paths[i:i+BATCH_SIZE]]
+        inputs = processor(images=images, return_tensors="pt").to(DEVICE)
+        out = model.generate(**inputs, max_new_tokens=500)
+        predictions.extend(processor.batch_decode(out, skip_special_tokens=True))
+    return predictions
 
 ds = load_dataset("ydshieh/coco_dataset_script", "2017", data_dir="/content/coco")
 
@@ -23,24 +56,31 @@ for m in ds['validation']:
     image_path_to_captions[image_path].append(caption)
   else:
     image_path_to_captions[image_path] = [caption]
-
-total_predictions = []
-total_references = []
-
 image_path_to_captions = list(image_path_to_captions.items())
-image_path_to_captions = [(item[0],item[1][:5]) for item in image_path_to_captions]
-for i in tqdm(range(0, len(image_path_to_captions), BATCH_SIZE)):
-    batch = image_path_to_captions[i:i+BATCH_SIZE]
-    images = [Image.open(item[0]) for item in batch]
-    inputs = BLIP_PROCESSOR(images=images, return_tensors="pt").to(DEVICE)
-    out = BLIP_MODEL.generate(**inputs, max_new_tokens=500)
-    total_predictions.extend(BLIP_PROCESSOR.batch_decode(out, skip_special_tokens=True))
-    total_references.extend([item[1] for item in batch])
+references = [item[1][:5] for item in image_path_to_captions]
+image_paths = [item[0] for item in image_path_to_captions]
 
-for metric_name in ['sacrebleu', 'rouge']:
-  metric = evaluate.load(metric_name)
-  results = metric.compute(predictions=total_predictions, references=total_references)
-  if metric_name == 'sacrebleu':
-    print(f"BLEU score: {results['score']}")
-  elif metric_name == 'rouge':
-    print(f"ROUGE score: {results}")
+total_predictions = {'file_name': list(map(os.path.basename, image_paths))}
+eval_results = []
+
+for model_name, initializer in MODEL_INITIALIZERS.items():
+    model, processor = initializer()
+    predictions = caption_images(model, processor, image_paths)
+    total_predictions[model_name] = predictions
+    for metric_name in ['sacrebleu', 'rouge', 'meteor']:
+        metric = evaluate.load(metric_name)
+        results = metric.compute(predictions=predictions, references=references)
+        if metric_name == 'sacrebleu':
+            eval_results.append((model_name, metric_name, results['score']))
+        elif metric_name == 'rouge':
+            for k, v in results.items():
+                eval_results.append((model_name, k, v))
+        elif metric_name == 'meteor':
+            eval_results.append((model_name, metric_name, results['meteor']))
+    cider_scorer = CiderScorer()
+    for pred, ref in zip(predictions, references):
+        cider_scorer += (pred, ref)
+    score, _ = cider_scorer.compute_score()
+    eval_results.append((model_name, 'CIDEr', score))
+
+print(eval_results)
